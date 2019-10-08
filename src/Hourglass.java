2f739755ed9c8ca5e4bba274063ea48ba1702555
yanchemm000 <yanchemm000@CAM-LAB-27.bgsd.k12.wa.us>
2019-10-08 07:48:12 -0700

    Needs work

--- src/Hourglass.java
+++ src/Hourglass.java
@@ -1,7 +1,18 @@
-/*
-
+/*This is my commit block where I am supposed to tell you how I plan to do this.
+Problem is, I don't know what I'm going to do yet. RETYPE LATER
 */
 
 public class Hourglass {
+	public static void main(String args[] ) {
+		topBottom();
+	}
+	public static void topBottom () {
+		System.out.println("|\"\"\"\"\"\"\"\"\"\"|");
+	}
+	public static void layers () {
+		for (int line=1; line < 5; line++) {
+			System.out.println("\\:/");
+		}
+	}
 
 }
