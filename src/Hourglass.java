a866faaace034097e6b9ba136885786372186253
yanchemm000 <yanchemm000@CAM-LAB-27.bgsd.k12.wa.us>
2019-10-10 07:49:16 -0700

    Still needs work

--- src/Hourglass.java
+++ src/Hourglass.java
@@ -5,14 +5,59 @@ Problem is, I don't know what I'm going to do yet. RETYPE LATER
 public class Hourglass {
 	public static void main(String args[] ) {
 		topBottom();
+		topLayers();
+		middle();
+		bottomLayers();
 	}
 	public static void topBottom () {
 		System.out.println("|\"\"\"\"\"\"\"\"\"\"|");
 	}
-	public static void layers () {
-		for (int line=1; line < 5; line++) {
-			System.out.println("\\:/");
+	public static void topLayers () {
+		/*this gives me the top half of the hour glass*/
+		for (int line=1; line <= 4; line++) {
+			/*This makes it so that it will add spaces before of 
+			 * the hour glass to give it its shape
+			 */
+			for (int i = 1; i<= (line); i++) {
+				System.out.print(" ");
+			}
+			/*this is for a line*/
+			System.out.print("\\");
+			for (int j = 1; j<=(10-(line * 2)); j++) {
+				System.out.print(":");
+			}
+			System.out.println("/");
+		}
+		
+		
+		
+	}
+	public static void middle () {
+		System.out.println("     ||");
+	}
+	public static void bottomLayers() {
+		for (int line = 1; line <= 4; line++) {
+			for (int i = 1; i<= ((line * 4)-1); line++) {
+				System.out.print(" ");
+			}
+			System.out.print("/");
+			for (int j = 1; j<= line * 2; line++) {
+				System.out.print(":");
+			}
+			System.out.println("\\");
+			
 		}
 	}
-
+	
 }
+
+
+
+
+
+
+
+
+
+
+
